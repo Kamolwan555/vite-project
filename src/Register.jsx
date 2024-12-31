e0618dc7cee1f9ee4f12a29c16736218bcb99b18
Kamolwan Sangnoo <pppor3811@gmail.com>
2024-12-31 17:28:37 +0700

    edit form

--- src/Register.jsx
+++ src/Register.jsx
@@ -12,8 +12,10 @@ export default function Register() {
     confirmPassword: "",
     address: "",
     village: "",
+    soi: "",
     road: "",
     province: "",
+    subdistrict: "",
     district: "",
     postcode: "",
   });
@@ -144,7 +146,7 @@ export default function Register() {
                   onChange={handleChange}
                   required
                   className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
-                  placeholder="กำหนดรหัสผ่านขั้นต่ำ 8 ตัว"
+                  placeholder="รหัสผ่าน"
                 />
               </div>
 
@@ -163,36 +165,36 @@ export default function Register() {
                   onChange={handleChange}
                   required
                   className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
-                  placeholder="กำหนดรหัสผ่านขั้นต่ำ 8 ตัว"
+                  placeholder="ยืนยันรหัสผ่าน"
                 />
               </div>
 
               <div>
-            <button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-            >
-              ถัดไป
-            </button>
-          </div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                >
+                  ถัดไป
+                </button>
+              </div>
 
-          <p className="mt-4 text-center text-sm text-gray-600">
-            มีบัญชีอยู่แล้ว?{" "}
-            <a
-              href="#"
-              className="font-medium text-green-600 hover:text-green-500"
-            >
-              เข้าสู่ระบบ
-            </a>
-          </p>
-        </form>
+              <p className="mt-4 text-center text-sm text-gray-600">
+                มีบัญชีอยู่แล้ว?{" "}
+                <a
+                  href="#"
+                  className="font-medium text-green-600 hover:text-green-500"
+                >
+                  เข้าสู่ระบบ
+                </a>
+              </p>
+            </form>
           </>
         )}
 
         {page === 2 && (
           <>
             <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
-              รายละเอียดเพิ่มเติม
+              ลงทะเบียน
             </h2>
             <form className="space-y-4" onSubmit={handleSubmit}>
               <div>
@@ -214,28 +216,144 @@ export default function Register() {
                 />
               </div>
 
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label
+                    htmlFor="village"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    หมู่ที่
+                  </label>
+                  <input
+                    id="village"
+                    name="village"
+                    type="text"
+                    value={formData.village}
+                    onChange={handleChange}
+                    className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
+                    placeholder="หมู่ที่"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="soi"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    ซอย
+                  </label>
+                  <input
+                    id="soi"
+                    name="soi"
+                    type="text"
+                    value={formData.soi}
+                    onChange={handleChange}
+                    className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
+                    placeholder="ซอย"
+                  />
+                </div>
+              </div>
+
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label
+                    htmlFor="soi"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    ถนน
+                  </label>
+                  <input
+                    id="road"
+                    name="road"
+                    type="text"
+                    value={formData.road}
+                    onChange={handleChange}
+                    className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
+                    placeholder="ถนน"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="provinces"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    จังหวัด
+                  </label>
+                  <input
+                    id="provinces"
+                    name="provinces"
+                    type="text"
+                    value={formData.provinces}
+                    onChange={handleChange}
+                    className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
+                    placeholder="จังหวัด"
+                  />
+                </div>
+              </div>
+
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label
+                    htmlFor="subdistrict"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    อำเภอหรือเขต
+                  </label>
+                  <input
+                    id="subdistrict"
+                    name="subdistrict"
+                    type="text"
+                    value={formData.subdistrict}
+                    onChange={handleChange}
+                    className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
+                    placeholder="อำเภอหรือเขต"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="district"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    ตำบลหรือแขวง
+                  </label>
+                  <input
+                    id="district"
+                    name="district"
+                    type="text"
+                    value={formData.district}
+                    onChange={handleChange}
+                    className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
+                    placeholder="ตำบลหรือแขวง"
+                  />
+                </div>
+              </div>
+
               <div>
                 <label
-                  htmlFor="village"
+                  htmlFor="postcode"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  หมู่
+                  รหัสไปรษณีย์
                 </label>
                 <input
-                  id="village"
-                  name="village"
+                  id="postcode"
+                  name="postcode"
                   type="text"
-                  value={formData.village}
+                  value={formData.postcode}
                   onChange={handleChange}
+                  required
                   className="block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-green-600 focus:border-green-600 sm:text-sm"
-                  placeholder="หมู่"
+                  placeholder="รหัสไปรษณีย์"
                 />
               </div>
 
+              <div className="grid grid-cols-2 gap-4">
               <button
                 type="button"
                 onClick={handleBack}
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-100 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-50"
               >
                 ย้อนกลับ
               </button>
@@ -245,6 +363,7 @@ export default function Register() {
               >
                 ลงทะเบียน
               </button>
+              </div>
             </form>
           </>
         )}
